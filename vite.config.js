37ab7c719c7dc980b37c0fb01d5c8b24be46c85f
Txnishi <Tanishidevlal@gmail.com>
2023-12-14 19:35:02 +0530

    LFUI

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Ensure that the output is not minified for easier debugging
+    minify: false,
+    // Set a base path if your app is not deployed at the root of the domain
+    base: "./",
+  },
+});
