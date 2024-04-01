32ab5540a89a653d6eb25dd4030b38fcb6584049
Txnishi <Tanishidevlal@gmail.com>
2024-04-01 16:29:59 +0530

    JdVVNL Dashboard

--- src/data/ChartData.jsx
+++ src/data/ChartData.jsx
@@ -22,4 +22,28 @@ export const getMonthlyChartData = async (selectedOptionId, value) => {
     }
 };
 
+export const getJdvnlDailyChartData = async (selectedOptionId, value) => {
+    try {
+        const res = await axios.get(`http://13.127.57.185:5000/getPredDataDaily?id=${selectedOptionId}&date=${value}`);
+        console.log(res.data);
+        return res.data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+};
+
+export const getJdvnlMonthlyChartData = async (selectedOptionId, value) => {
+    try {
+        const res = await axios.get(`http://13.127.57.185:5000/getPredDataMonthly?id=${selectedOptionId}&date=${value}`);
+        console.log(res.data);
+        return res.data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+};
+
+
+
 // export default getChartData;
